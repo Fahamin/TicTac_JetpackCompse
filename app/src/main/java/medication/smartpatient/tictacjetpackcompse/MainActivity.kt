c70833e8835fff2d51bb18958cb0780b7f295b9c
Fahamin <fahamin00@gmail.com>
2023-04-08 16:52:18 +0600

    COMPLETE

--- app/src/main/java/medication/smartpatient/tictacjetpackcompse/MainActivity.kt
+++ app/src/main/java/medication/smartpatient/tictacjetpackcompse/MainActivity.kt
@@ -7,13 +7,11 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -23,7 +21,16 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 import medication.smartpatient.tictacjetpackcompse.ui.theme.TicTacJetpackCompseTheme
+import kotlin.random.Random
+
+enum class Win {
+    Player, AI,
+    Draw
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,23 +55,111 @@ fun tttScreen() {
         mutableStateOf(true)
     }
     var moves = remember {
-        mutableStateListOf<Boolean?>(true, false, null, false, true, null, null, null)
+        mutableStateListOf<Boolean?>(
+            true, null, false, null, true, false, null, null, null
+        )
     }
 
+    val win = remember {
+        mutableStateOf<Win?>(null)
+    }
     val onTap: (Offset) -> Unit = {
         if (playerTurn.value) {
             val x = (it.x / 333).toInt()
             val y = (it.y / 333).toInt()
-
+            val posInMoves = y * 3 + x;
+            if (moves[posInMoves] == null) {
+                moves[posInMoves] = true
+                playerTurn.value = false
+            }
         }
     }
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
 
         Text(text = "Tic Tac Tok")
         Head(playerTurn.value)
-        Board(moves)
+        Board(moves, onTap)
+
+        if (!playerTurn.value) {
+            CircularProgressIndicator(color = Color.Red, modifier = Modifier.padding(13.dp))
+
+            val coroutineScope = rememberCoroutineScope()
+
+            LaunchedEffect(key1 = Unit)
+            {
+                coroutineScope {
+                    delay(1500L)
+                    while (true) {
+                        val i = Random.nextInt(9)
+                        if (moves[i] == null) {
+                            moves[i] = false
+                            playerTurn.value = true
+                            win.value = checkEnd(moves)
+                            break
+                        }
+                    }
+                }
+            }
+
+        }
+        if (win.value != null) {
+            when (win.value) {
+                Win.Draw -> {
+                    Text(text = "DRAW")
+
+                }
+                Win.Player -> {
+                    Text(text = "Player has won")
+                }
+                Win.AI -> {
+                    Text(text = "AI has won")
+
+                }
+                else -> {}
+            }
+        }
     }
 
+}
+
+
+fun checkEnd(m: List<Boolean?>): Win? {
+    var win: Win? = null
+    if ((m[0] == true && m[1] == true && m[2] == true) ||
+        (m[3] == true && m[4] == true && m[5] == true) ||
+        (m[6] == true && m[7] == true && m[8] == true) ||
+        (m[0] == true && m[3] == true && m[6] == true) ||
+        (m[1] == true && m[4] == true && m[7] == true) ||
+        (m[2] == true && m[5] == true && m[8] == true) ||
+        (m[0] == true && m[4] == true && m[8] == true) ||
+        (m[2] == true && m[4] == true && m[6] == true)
+
+    )
+        win = Win.Player
+    if ((m[0] == false && m[1] == false && m[2] == false) ||
+        (m[3] == false && m[4] == false && m[5] == false) ||
+        (m[6] == false && m[7] == false && m[8] == false) ||
+        (m[0] == false && m[3] == false && m[6] == false) ||
+        (m[1] == false && m[4] == false && m[7] == false) ||
+        (m[2] == false && m[5] == false && m[8] == false) ||
+        (m[0] == false && m[4] == false && m[8] == false) ||
+        (m[2] == false && m[4] == false && m[6] == false)
+
+    )
+        win = Win.AI
+
+    if (win == null) {
+        var available = false
+        for (i in 0..8) {
+            if (m[i] == null) {
+                available = true
+            }
+            if (!available) {
+                win = Win.Draw
+            }
+        }
+    }
+    return win
 }
 
 @Composable
